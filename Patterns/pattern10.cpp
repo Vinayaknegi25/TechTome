137a8184fe1752e2f7775a7d00614838585c9ff5
“Vinayaknegi25” <95993782+Vinayaknegi25@users.noreply.github.com>
2024-03-03 14:33:15 +0530

    3 more patterns

---
+++ Patterns/pattern10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch=65;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<ch;
+        }
+        ch++;
+        cout<< endl;
+    }
+}
